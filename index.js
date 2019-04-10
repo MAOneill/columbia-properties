cea6bd8222e3afce3f31382f3af143e11c7f31ed
Margaret O'Neill <margaret@oneillfish.com>
2019-04-10 13:52:29 -0400

    login page beginnings

--- index.js
+++ index.js
@@ -1,132 +1,42 @@
-//see comments for the web server in /hello-http/index.js
-const http = require('http');
-const fs = require('fs');  
+require('dotenv').config();
+const express = require('express');
+const app = express();
 
-const hostname = '127.0.0.1';
-const port = 3000;
+//this will hide that we are using express in our html headers
+//security meausre
+const helmet = require('helmet');
+app.use(helmet());
 
-const querystring = require('querystring');
+//for rendering html pages to user from client
+const es6Renderer = require('express-es6-template-engine');
+app.engine('html',es6Renderer);
+app.set('views','./views');
+app.set('view engine','html');
 
-//pull in your restaurants model so we can use it:
-//import my model class
-const Restaurant = require('./models/restaurants');   //pulls in restaurants.js models
-const User = require('./models/user.js');
-//helper function - this is our middleware
-//aka request handlers (similar to event handlers)
-const server = http.createServer(async (req,res) => {
-    const method = req.method;
-    console.log(req.url);
-    res.statusCode = 200;
-    // res.setHeader('Content-type', 'text/plain');
-    res.setHeader('Content-type', 'application/json');  //for our json data
+//to save values to session folder
+const session = require('session');
+const FileStore = require('session-file-store')(session);
 
+app.use(session ( {
+    store: new FileStore(),
+    secret: process.env.SESSION_SECRET
+}));
 
-            //if req.url = /restuarants - send them all restaurants
-            //if req.url = /use3rs - send a list of users etc...
-            //else - send a welcome message (your default index.html)
-            if(req.url === "/restuarants") {
-                //returns them each as an array of javascript objects
-                //because we use AWAIT, the hander function (const server) is ASYNC!
-                if (method === "GET"){
-                    const allRestaurants = await Restaurant.getAll(); 
-                    res.setHeader('Content-type', 'text/html');
-    
-                    //convert the array of objects into a string...
-                    const restaurantsJSON = JSON.stringify(allRestaurants);
-                    res.end(`<a href="/index">home</a><br/>` + restaurantsJSON);
-                    }
-                else if (method === "POST") {
-                    res.end ("No soup POST for you");
-                }
-                else if (method === "PUT"){
-                    res.end ("No soup PUT for you");
-                }
-                else if (method === "DELETE") {
-                    res.end ("No soup DELETE for you");
-                }
-        
-            }
+const loginRouter = require('./routes/login');
 
+//allows form parameters values to be stored in req.body on post
+app.use(express.urlencoded({extended:true}));
 
-    
-    else if(req.url.startsWith("/users")) {
-        const parts  = req.url.split("/");
-        // console.log(parts);
-        //get number
-        // const userId = parts[parts.length-1];
+//tells where to process which http request
+app.use('/login',loginRouter);
 
-        if (method === "GET"){
-            if (parts.length > 3) {
-                res.statusCode = 404;
-                res.setHeader('Content-type', 'text/html');
-                // res.end("404 resourse not found");
-                res.end(` <a href="/index">home</a><br/> + <h1>404 resourse not found</h1><a href="./restuarants">restuarants</a><br><a href="./users">users</a>`);
-    
-            }
-            else if (parts.length === 2)  {
-             //get all data
-                const allUsers = await User.getAll();
-                const userJSON = JSON.stringify(allUsers);
-                // res.end(userJSON);
-                res.setHeader('Content-type', 'text/html');
-    
-                res.end(`<a href="/index">home</a><br/>` + userJSON);
-    
-            }
-            else if (parts.length === 3) {
-            //get just a particular user info
-                const userId = parts[2];
-                const theUser = await User.getById(parseInt(parts[2]));
-                console.log(theUser);
-                const userJSON = JSON.stringify(theUser);
-                res.setHeader('Content-type', 'text/html');
-                res.end(`<a href="/index">home</a><br>` + userJSON);
-            }
-        }
-        else if (method === "POST"){
-            let body = "";
-            req.on('data', (chunk) => {
-                body += chunk.toString();
-            })
-            req.on('end',() => {
-                console.log("The body looks like this", body);
-                const parsedBody = querystring.parse(body);
-                const newUserId = User.add(parsedBody);  //this returns an id
-
-                res.end ("You posted something")
-
-            })
-
-        }
-        else if (method === "PUT"){
-            res.end ("No soup PUT for you")
-        }
-        else if (method === "DELETE"){
-            if (parts.length === 3) {
-                const userId = parts[2];
-                await User.delete(userId);
-                //but you can only delete users that don't have 
-                //records in the favorits or reviews table
-                res.end(`{"message":"deleted user ${userId}}"`)
-            }
-            res.end ("No soup DELETE for you")
-        }
-        
-    }
-    else   {
-        //format the response as json, b/c that is our content type.
-        //this works:
-        // res.end(` <a href="./restuarants">restuarants</a><br><a href="./users">users</a>`);
-        // res.end(`{message:"Thank you for your patronage.  Please send bitcoin.}`);
-        //but so does this:
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        fs.createReadStream('index.html').pipe(res);
-    }
+//catch all
+app.use('*', (req, res) => {
+    res.status('404').send('We do not have a page for that yet.');
 });
 
 
-
-//turn the server on
-server.listen(port, hostname, () => {
-    console.log(`Server is running at http://${hostname}:${port}.`);
+app.listen(process.env.PORT ,() => {
+    console.log(`columbia prop app running on port: ${process.env.PORT}.`);
 });
+
---
+++ routes/login.js
@@ -0,0 +1,51 @@
+//pulls in the user model
+const User = require('./models/users');
+
+function showLoginPage(req,res) {
+    //no locals yet
+    res.render('login')
+}
+
+async function verifyUser  (req, res) {
+    //set session email
+    req.session.userid = req.body.userid;
+
+    req.session.save( async () => { 
+
+        //get the email from the post body
+        // console.log(req.body.email);
+        const theUser = await User.getById(`'${req.body.userid}'`);
+    
+        //if the user not found, redirect to the login page
+        //in this app, they can't create login accounts
+        if (theUser === null) {
+            //you could set message...
+            res.redirect('/login');
+        }
+        else {  //user exists in user table
+
+            //if the user exists, check password
+            if (theUser.checkPassword(req.body.password)) {
+                console.log("PASSWORD VALID");
+                req.session.userId = theUser.id;
+
+                //if valid, let user into main page
+                req.session.save( () => {
+                    res.render('main',{locals:{userid:req.session.userId}});
+                })
+            }
+            //wrong password
+            else {
+                //code to hash the password
+                await theUser.setPassword(req.body.password);
+                console.log("saved the password");
+                await theUser.save();
+                console.log("updated the db");
+        // res.render('login',{locals:{email:req.body.email, message:"password incorrect. please try again"}});  //this will be a Get
+                res.render('login');
+                }
+        }
+    })
+
+    ;
+}
